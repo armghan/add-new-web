6ab4814c3b3cd7075a026469a437ceaaf6b97671
armghan <armughanmb@outlook.com>
2022-02-20 02:02:44 +0500

    SSL Implimentation

--- add-new-app.bash
+++ add-new-app.bash
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 sitename=$1
-containername=$2
 
 cp siteX.conf nginx-conf/$sitename.conf
 sed -i "s/siteX/$sitename/g" nginx-conf/$sitename.conf
 
 cp new-web-container-template new-container
 sed -i "s/siteX/$sitename/g" new-container
-sed -i "s#IMAGE#$containername#g" new-container
+cp .env-Example .env-$sitename
+sed -i "s/siteX/$sitename/g" .env-$sitename
+mkdir ENV/env-$sitename
+cp .env-$sitename ENV/env-$sitename/.env-$sitename
 cat new-container >> docker-compose.yml
 
 rm -f new-container
-
-docker-compose up -d
-docker restart reverse-proxy
+rm -f .env-$sitename
+#docker-compose up -d
+#docker restart reverse-proxy
